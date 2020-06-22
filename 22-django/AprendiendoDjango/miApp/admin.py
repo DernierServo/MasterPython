18779282936b10810e2763e1f3e36eca26871e84
dservo <arrasco.cs@gmail.com>
2020-06-22 11:39:27 -0500

    Admin en Django y configuración de Modelos.

--- 22-django/AprendiendoDjango/miApp/admin.py
+++ 22-django/AprendiendoDjango/miApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Article, Category
 
 # Register your models here.
+# Ds: Para generar un CRUD de cada modelo en el /admin de Django.
+admin.site.register(Article)
+admin.site.register(Category)
