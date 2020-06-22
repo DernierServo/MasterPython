e6604ef0e867220f69ad65374eae47dcc2731bda
dservo <arrasco.cs@gmail.com>
2020-06-22 11:56:32 -0500

    Sobrenombre y ordenamiento en modelos para Administración Django

--- 22-django/AprendiendoDjango/miApp/models.py
+++ 22-django/AprendiendoDjango/miApp/models.py
@@ -9,10 +9,22 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        verbose_name = "Articulo"
+        verbose_name_plural = "Artículos"
+        #ordering = ['-id', 'created_at']
+        #ordering = ['title']
+        ordering = [id]
+
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     created_at = models.DateTimeField()
+
+    class Meta:
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorías"
 
 
