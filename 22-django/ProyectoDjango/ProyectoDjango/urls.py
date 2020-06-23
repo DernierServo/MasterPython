6c53a34e0a8af56c2e549c74b702698422d5e91b
dservo <arrasco.cs@gmail.com>
2020-06-23 17:11:29 -0500

    Agregando views, templates y configurando urls.py

--- 22-django/ProyectoDjango/ProyectoDjango/urls.py
+++ 22-django/ProyectoDjango/ProyectoDjango/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='n_index'),
+    path('inicio/', views.index, name='n_inicio'),
+    path('sobre-nosotros/', views.about, name='n_about'),
 ]
