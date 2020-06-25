1f1b9b543cf60c41056d5af102c57dc82a503673
dservo <arrasco.cs@gmail.com>
2020-06-25 18:29:52 -0500

    Mostrar error 404

--- 22-django/ProyectoDjango/blog/views.py
+++ 22-django/ProyectoDjango/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Category, Article
 
 # Create your views here.
@@ -17,7 +17,7 @@ def list(request):
 
 
 def category(request, p_category_id):
-    category = Category.objects.get(id=p_category_id)
+    category = get_object_or_404(Category, id=p_category_id)
 
     return render(
         request,
