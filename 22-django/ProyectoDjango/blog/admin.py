0b895b5e4f10070c4d46b80e731f6a0272e92cfa
dservo <arrasco.cs@gmail.com>
2020-06-26 16:21:00 -0500

    Personalizar listados en el Panel de Configuración de Django

--- 22-django/ProyectoDjango/blog/admin.py
+++ 22-django/ProyectoDjango/blog/admin.py
@@ -1,16 +1,27 @@
 from django.contrib import admin
 from .models import Category, Article
 
+
 class CategoryAdmin(admin.ModelAdmin):
-    readonly_fields = ('created_at',)
+    readonly_fields = ('created_at',)  #ds: agrega campos de lectura
+    search_fields = ('name', 'description')    #ds: Agrega buscador
+    list_display = ('name', 'description', 'created_at')   #Mostrar en columnas de vista previa
+    ordering = ('-created_at',)    
+
 
 class ArticleAdmin(admin.ModelAdmin):
-    readonly_fields = ('user', 'created_at', 'updated_at')
+    readonly_fields = ('user', 'created_at', 'updated_at')  #ds: agrega campos de lectura
+    search_fields = ('title', 'content', 'user__username', 'categories__name')    #ds: Agrega buscador
+    list_filter = ('public', 'user__username', 'categories__name')  #Filtro para búsqueda
+    list_display = ('title', 'user', 'image', 'public', 'created_at')   #Mostrar en columnas de vista previa
+    ordering = ('-created_at',)    
 
+    # Para guardar el usuario de la sesión en el campo usuario del modelo Article
     def save_model(self, request, obj, form, change):
         if not obj.user_id:
             obj.user_id = request.user.id
         obj.save()
+
 
 # Register your models here.
 admin.site.register(Category, CategoryAdmin)
--- 22-django/ProyectoDjango/pages/admin.py
+++ 22-django/ProyectoDjango/pages/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Page
 
+# ds: Configuración extra
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')  #ds: agrega campos de lectura
+    search_fields = ('title', 'content')    #ds: Agrega buscador
+    list_filter = ('visible',)  #Filtro para búsqueda
+    list_display = ('title', 'visible', 'created_at')   #Mostrar en columnas de vista previa
+    ordering = ('-created_at',)
+
 # Register your models here.
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
 
 # ds: Configuración del Panel
 title = "Proyecto con Django"
