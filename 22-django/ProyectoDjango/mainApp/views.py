7d64f1f522277d3d1c3b06f098beedd6f61a3ba8
dservo <arrasco.cs@gmail.com>
2020-06-27 16:46:55 -0500

    Mostrar usuario identificado

--- 22-django/ProyectoDjango/mainApp/views.py
+++ 22-django/ProyectoDjango/mainApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from .forms import RegisterForm
-
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def index(request):
@@ -49,6 +49,19 @@ def register_page(request):
 
 
 def login_page(request):
+
+    # Valida que la información esté llegando desde el Formulario: Método POST
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('n_inicio')
+        else:
+            messages.warning(request, 'No te has podido identificarsh!')
 
     return render(
         request, 
